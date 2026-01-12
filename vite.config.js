c6a7fdf8a98f8e09f380dc80c101d482fc4e2e74
Satyam Wayal <satyamwayalofficial@gmail.com>
2026-01-12 19:47:40 +0530

    Fix Vite base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-
-// vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    allowedHosts: ['865229773e0c.ngrok-free.app']
-  }
-})
-
+  plugins: [react()],
+  base: "/",   // 👈 ADD THIS LINE
+});
